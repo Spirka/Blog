33eb1ea5025ce0c6bb788a80b1f6ee45e59517ed
Kseniya Dergunova <kdergunova@vocalize.ru>
2020-08-20 21:09:41 +0300

    v. 0.0.2 setting post page

--- src/app/admin/shared/services/auth.service.ts
+++ src/app/admin/shared/services/auth.service.ts
@@ -1,20 +1,25 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
 import {FbAuthResponse, User} from '../../../shared/interfaces';
-import {Observable} from 'rxjs';
+import {Observable, Subject, throwError} from 'rxjs';
 import {environment} from '../../../../environments/environment';
-import {tap} from 'rxjs/operators';
+import {catchError, tap} from 'rxjs/operators';
 
 @Injectable()
 export class AuthService {
+
+  public error$: Subject<string> = new Subject<string>();
+
   constructor(private http: HttpClient) {
   }
+
   get token(): string {
     const expDate = new Date(localStorage.getItem('fb-token-exp'));
     if (new Date() > expDate) {
       this.logout();
+      return null;
     }
-    return localStorage.getItem('fbToken');
+    return localStorage.getItem('fb-token');
   }
 
   login(user: User): Observable<any> {
@@ -22,7 +27,8 @@ export class AuthService {
     return this.http.post(
       `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.apiKey}`, user)
       .pipe(
-        tap(resp => this.setToken(resp))
+        tap(this.setToken),
+        catchError(this.handleError.bind(this))
       );
   }
 
@@ -32,6 +38,23 @@ export class AuthService {
 
   isAuthenticated(): boolean {
     return !!this.token;
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    const {message} = error.error.error;
+
+    switch (message) {
+      case 'INVALID_EMAIL':
+        this.error$.next('Неверный email');
+        break;
+      case 'INVALID_PASSWORD':
+        this.error$.next('Неверный password');
+        break;
+      case 'EMAIL_NOT_FOUND':
+        this.error$.next('Такого email нет');
+        break;
+    }
+    return throwError(error);
   }
 
   private setToken(response: FbAuthResponse | null) {
